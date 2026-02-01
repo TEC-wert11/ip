8e716e4558ff87fc03d2caa985b6e7101f3b13ef
TEC-wert11 <e1122558@u.nus.edu>
2026-02-01 17:26:05 +0800

    level 2: add, list

--- src/main/java/Wertinator.java
+++ src/main/java/Wertinator.java
@@ -1,24 +1,54 @@
+import java.util.ArrayList;
 import java.util.Scanner ;
 
 public class Wertinator {
+
+    private ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
+        new Wertinator().run();
+    }
+
+    private void run(){
         System.out.println("Wassup guys! Wuchu guys doin? \n"
         + "This is Wertinator, back doing some more werting action! \n"
-        + "What are we doing today? \n" );
+        + "What ya wanna do today?" );
+        printALine();
 
         Scanner input = new Scanner(System.in);
-        boolean bye = false ;
 
-        while(!bye) {
+        while(true) {
             String line = input.nextLine();
-            System.out.println(line);
 
             if(line.equals("bye")){
-                bye = true;
+                break;
+            }
+
+            if (line.equals("list")){
+                this.printList();
+            }
+            else{
+                this.list.add(line);
+                printALine();
+                System.out.println("added: "+line);
+                printALine();
             }
         }
 
-        System.out.println("See you guys next time. \n"
+        printALine();
+        System.out.println("See you next time. \n"
         + "Peace out. \n" );
+    }
+
+    public void printList(){
+        printALine();
+        for (int i=1 ; i < list.size()+1 ; i++){
+            System.out.println( i + ". " + list.get(i-1));
+        }
+        printALine();
+    }
+
+    public void printALine(){
+        System.out.println("____________________________________________________________");
     }
 }
