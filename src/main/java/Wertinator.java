fa301420d1d14e2b7446d92a929b6e68c74b8a9f
TEC-wert11 <e1122558@u.nus.edu>
2026-02-01 15:02:28 +0800

    level 0: rename chatbot and print greeting

---
+++ src/main/java/Wertinator.java
@@ -0,0 +1,10 @@
+public class Wertinator {
+    public static void main(String[] args) {
+        System.out.println("Wassup guys! Wuchu guys doin? \n"
+        + "This is Wertinator, back doing some more werting action! \n"
+        + "What are we doing today? \n" );
+
+        System.out.println("See you guys next time. \n"
+        + "Peace out. \n" );
+    }
+}
