b5b55128e859336875098fe2b961d045d83c99ac
TEC-wert11 <e1122558@u.nus.edu>
2026-02-01 15:29:03 +0800

    level 1: echo

--- src/main/java/Wertinator.java
+++ src/main/java/Wertinator.java
@@ -1,8 +1,22 @@
+import java.util.Scanner ;
+
 public class Wertinator {
     public static void main(String[] args) {
         System.out.println("Wassup guys! Wuchu guys doin? \n"
         + "This is Wertinator, back doing some more werting action! \n"
         + "What are we doing today? \n" );
+
+        Scanner input = new Scanner(System.in);
+        boolean bye = false ;
+
+        while(!bye) {
+            String line = input.nextLine();
+            System.out.println(line);
+
+            if(line.equals("bye")){
+                bye = true;
+            }
+        }
 
         System.out.println("See you guys next time. \n"
         + "Peace out. \n" );
